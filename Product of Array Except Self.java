eb74284202ab8572063e22ff144cd2914703d2a6
Abhik-k <112130293+Abhik-k@users.noreply.github.com>
2025-02-24 01:12:16 +0530

    Update Product of Array Except Self.java
    Modified approach

--- Product of Array Except Self.java
+++ Product of Array Except Self.java
@@ -29,3 +29,26 @@ class Solution {
         return output;
     }
 }
+
+
+//Using Only prefix array and right variable to store postfix multiples
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int prefix[]=new int[n];
+        int output[]=new int[n];
+        prefix[0]=1;
+        for(int i =1;i<n;i++){
+            prefix[i]=prefix[i-1]*nums[i-1];
+        }
+        System.out.println(Arrays.toString(prefix));
+        int right=1;
+
+        for(int i=n-1;i>=0;i--){
+            output[i]=prefix[i]*right;
+            right*=nums[i];
+        }
+
+        return output;
+    }
+}
